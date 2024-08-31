e5669158a01767e00d38fe539d2a83a9d4fcd99e
Rajkumar-pro <itsrks861@gmail.com>
2024-08-31 10:19:06 +0530

    update

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('Hello World!');
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('nav ul li a');
+    links.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const sectionId = this.getAttribute('href').substring(1);
+            const section = document.getElementById(sectionId);
+            window.scrollTo({
+                top: section.offsetTop,
+                behavior: 'smooth'
+            });
+        });
+    });
+});
